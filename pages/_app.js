886f9526b23bcafc1eb11a551d2549158aefe823
ben40d9 <benpaley6@gmail.com>
2022-07-26 21:13:38 -0700

    changes in class 7/26

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import "@fontsource/catamaran"
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Catamaran', sans-serif`,
+    body: `'Catamaran', sans-serif`
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
