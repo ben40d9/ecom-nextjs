e7d71129e1c8d2eb3bf3e50b846a530b8e807cc3
ben40d9 <benpaley6@gmail.com>
2022-07-28 14:31:13 -0700

    box inside of the grid Element, that is suppposed to show our products

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { 
-  Flex, 
+import {
+  Flex,
   Text,
   Box,
   Heading,
@@ -7,69 +7,65 @@ import {
   Spacer,
   ButtonGroup,
   Grid,
-  GridItem
-} from '@chakra-ui/react';
+  GridItem,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Grid
-  templateAreas={`
-    "header header"
-    "hero banner"
-    "nav nav"
-    "main lain"
-    "footer footer"
-  `}
-  gridTemplateRows={'50px 1fr 30px 30px'}
-  gridTemplateColumns={''}
-  h='500px'
-  gap='2'
-  color='blackAlpha.700'
-  fontWeight='bold'
->
-  <GridItem area={'header'}>
-    <Flex 
-      bgGradient='linear(to-r, green.200, red.400)' 
-      justifyContent={'center'} 
-      alignItems={'center'}
+      templateAreas={`
+        "header header"
+        "hero hero"
+        "nav nav"
+        "main lain"
+        "footer footer"
+      `}
+      gridTemplateRows={"50px 1fr 90px 70px 50px"}
+      gridTemplateColumns={"150px 1"}
+      h="800px"
+      gap="2"
+      color="blackAlpha.700"
+      fontWeight="bold"
     >
-      <Box p={'2'}>
-        <Heading size={'lg'}>
-          BCA Eshop
-        </Heading>
-      </Box>
-      <Spacer />
-      <ButtonGroup gap={'2'}>
-        <Button colorScheme={'alphaWhite'} variant={'outline'}>Sign up</Button>
-        <Button mr={4} colorScheme={'alphaWhite'} variant={'outline'}>Log In</Button>
-      </ButtonGroup>
-    </Flex>
-  </GridItem>
-  <GridItem pl='2' bg='yellow.300' colSpan={2} rowSpan={1} area={'hero'}>
-    Hero Banner
-  </GridItem>
-  <GridItem pl='2' bg='pink.300' area={'nav'}>
-    <Flex>
-      <Box p={'2'}>
-        wallet
-      </Box>
-      <Box p={'2'}>
-        cup
-      </Box>
-      <Box p={'2'}>
-        tshirt
-      </Box>
-    </Flex>
-  </GridItem>
-  <GridItem pl='2' bg='green.300' area={'main'}>
-    div_box#1
-  </GridItem>
-  <GridItem pl='2' bg='green.300' area={'lain'}>
-    div_box#2
-  </GridItem>
-  <GridItem pl='2' bg='blue.300' area={'footer'}>
-    Footer
-  </GridItem>
-</Grid>
-  )
+      <GridItem area={"header"}>
+        <Flex
+          bgGradient="linear(to-r, green.200, red.400)"
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Box p={"2"}>
+            <Heading size={"lg"}>BCA Eshop</Heading>
+          </Box>
+          <Spacer />
+          <ButtonGroup gap={"2"}>
+            <Button colorScheme={"alphaWhite"} variant={"outline"}>
+              Sign up
+            </Button>
+            <Button mr={4} colorScheme={"alphaWhite"} variant={"outline"}>
+              Log In
+            </Button>
+          </ButtonGroup>
+        </Flex>
+      </GridItem>
+      <GridItem pl="2" bg="yellow.300" colSpan={2} rowSpan={1} area={"hero"}>
+        Hero Banner
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        <Flex justifyContent={"center"} alignItems={"center"}>
+          <Box p={"2"}>wallet</Box>
+          <Box p={"2"}>cup</Box>
+          <Box p={"2"}>tshirt</Box>
+        </Flex>
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        div_box#1
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"lain"}>
+        div_box#2
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
