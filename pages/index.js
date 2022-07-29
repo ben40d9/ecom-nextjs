651a75c70b00658ed3295db6460ee433e95237f5
ben40d9 <benpaley6@gmail.com>
2022-07-28 22:32:45 -0700

    re-configured home page

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,6 @@
-import { 
-  Flex, 
-  Text,
-  Box,
-  Heading,
-  Button,
-  Spacer,
-  ButtonGroup
-} from '@chakra-ui/react';
+import HomeLayout from "../components/layouts/HomeLayout";
+import ProductDetails from "../components/sections/ProductDetails";
 
 export default function Home() {
-  return (
-    <Flex 
-      bgGradient='linear(to-r, green.200, red.400)' 
-      justifyContent={'center'} 
-      alignItems={'center'}
-    >
-      {/* <Text fontSize={'4xl'}>
-        BCA eShop
-      </Text> */}
-      <Box p={'4'}>
-        <Heading size={'lg'}>
-          BCA Eshop
-        </Heading>
-      </Box>
-      <Spacer />
-      <ButtonGroup gap={'2'}>
-        <Button colorScheme={'alphaWhite'} variant={'outline'}>Sign up</Button>
-        <Button mr={'4'} colorScheme={'alphaWhite'} variant={'outline'}>Log In</Button>
-      </ButtonGroup>
-    </Flex>
-  )
+  return <HomeLayout children={<ProductDetails />} />;
 }
